d1bb90f4b09a5e09f933e01bb82ce7c810e22e55
Balthazar Rouberol <rouberol.b@gmail.com>
2012-02-23 13:25:58 +0100

    Code cleaning : better names, more comments
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- webcamviewer.py
+++ webcamviewer.py
@@ -13,6 +13,7 @@ pygst.require("0.10")
 import gst
 import argparse
 import Image
+from time import sleep
 
 class Webcam:
     
@@ -21,6 +22,18 @@ class Webcam:
         Set up the GUI, the gstreamer pipeline and webcam<-->GUI communication bus.
         If any, the external webcam (/dev/video1) will be selected by default, otherwise, /dev/video0 will be used
         """
+
+        # OBJECT ARGUMENTS
+        self.device = device         
+        self.W = resolution[0].split(':')[0]
+        self.H = resolution[0].split(':')[1]
+        self.framerate = framerate
+        self.snap_format = snap_format
+        if self.snap_format == 'png':
+            self.image_enc = 'pngenc'
+        elif self.snap_format in ['jpg','jpeg']:
+            self.image_enc = 'jpegenc'
+
         # SET UP THE INTERFACE
         window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         window.set_title("Webcam-Viewer")
@@ -35,29 +48,19 @@ class Webcam:
         hbox.set_border_width(10)
         hbox.pack_start(gtk.Label())
         self.button = gtk.Button("Snap")
-        self.button.connect("clicked", self.get_frame)
+        self.button.connect("clicked", self.get_snapshot)
         hbox.pack_start(self.button, False)
         hbox.add(gtk.Label())
         
-        # SET UP THE GSTREAMER PIPELINE 
-        self.device = device         
-        self.W = resolution[0].split(':')[0]
-        self.H = resolution[0].split(':')[1]
-        self.framerate = framerate
-        self.snap_format = snap_format
-        if self.snap_format == 'png':
-            self.image_enc = 'pngenc'
-        elif self.snap_format in ['jpg','jpeg']:
-            self.image_enc = 'jpegenc'
+        # SET UP THE GSTREAMER VIDEO PIPELINE 
         print 'v4l2src device={0} ! video/x-raw-yuv,width={1},height={2},framerate={3}/1 ! xvimagesink '.format(self.device, str(self.W), str(self.H), str(self.framerate))
-        self.player = gst.parse_launch('v4l2src device=%s ! video/x-raw-yuv,width=%s,height=%s,framerate=%s/1 ! xvimagesink name=videosink' %(self.device, self.W, self.H, self.framerate))
-        self.sink = self.player.get_by_name('videosink')
+        self.video_player = gst.parse_launch('v4l2src device=%s ! video/x-raw-yuv,width=%s,height=%s,framerate=%s/1 ! xvimagesink' %(self.device, self.W, self.H, self.framerate))
         # input from webcam (v4l2src : video for linux 2), output : xvimagesink
-        self.player.set_state(gst.STATE_PLAYING)
+        self.video_player.set_state(gst.STATE_PLAYING)
         # More information here : http://www.cin.ufpe.br/~cinlug/wiki/index.php/Introducing_GStreamer
 
         # SET UP WINDOW <--> WEBCAM STREAM COMMUNICATION BUS
-        bus = self.player.get_bus()
+        bus = self.video_player.get_bus()
         bus.add_signal_watch()
         bus.connect("message", self.on_message)
         bus.enable_sync_message_emission()
@@ -68,26 +71,20 @@ class Webcam:
                    
     def exit(self, widget, data=None):
         """ Exit the program """
-        self.player.set_state(gst.STATE_PAUSED) # Not sure if the PAUSE+READY steps are necessary, but when i use gst in bash, they are triggered.
-        self.player.set_state(gst.STATE_READY)
-        self.player.set_state(gst.STATE_NULL) 
-
-        # Ugly hack : launch cheese to avoid empty webcam window next time, probably due to webcam driver problem
-        # from os import system
+        self.video_player.set_state(gst.STATE_NULL) 
         gtk.main_quit()
-        # system("cheese")
         
     def on_message(self, bus, message):
         """ Gst message sink. Closes the pipeline in case of error or EOS (end of stream) message """
         t = message.type
         if t == gst.MESSAGE_EOS:
             print "MESSAGE EOS"
-            self.player.set_state(gst.STATE_NULL)
+            self.video_player.set_state(gst.STATE_NULL)
         elif t == gst.MESSAGE_ERROR:
             print "MESSAGE ERROR"
             err, debug = message.parse_error()
             print "Error: %s" % err, debug
-            self.player.set_state(gst.STATE_NULL)
+            self.video_player.set_state(gst.STATE_NULL)
                     
     def on_sync_message(self, bus, message):
         """ Webcam <--> GUI messages sink """
@@ -98,25 +95,28 @@ class Webcam:
             # Assign the viewport
             imagesink = message.src
             imagesink.set_property("force-aspect-ratio", True)
-            imagesink.set_xwindow_id(self.movie_window.window.xid) # HERE I SEND THE WEBCAM STREAM TO THE MOVIE WINDOW
+            imagesink.set_xwindow_id(self.movie_window.window.xid) # Sending video stream to gtk DrawingArea
 
-    def get_frame(self, e):
-        self.player.set_state(gst.STATE_PAUSED)
-        self.player.set_state(gst.STATE_NULL)    
+    def get_snapshot(self, e):
+        """ Capture a snapshot from webcam input """
+        # We close video stream
+        self.video_player.set_state(gst.STATE_NULL)
+        # Open a image capture pipeline
         snap_pipeline = "v4l2src device=/dev/video1 ! video/x-raw-yuv,width=%s,height=%s ! ffmpegcolorspace  ! %s ! filesink location=snapshot.%s" %(self.W, self.H, self.image_enc, self.snap_format)
         print snap_pipeline
-        self.player2= gst.parse_launch(snap_pipeline)
-        self.player2.set_state(gst.STATE_PLAYING)
-        import time
-        print "sleeping..."
-        time.sleep(0.5) #Hack : the player needs some time to take the picture. Otherwise, the snaphsot is empty
-        print "wake up!"
-        self.player2.set_state(gst.STATE_PAUSED)
-        self.player2.set_state(gst.STATE_NULL)
-        self.player.set_state(gst.STATE_PLAYING)
+        self.image_capture= gst.parse_launch(snap_pipeline)
+        self.image_capture.set_state(gst.STATE_PLAYING)
+        # Picture capture : Hack : it needs some time to take the picture, otherwise, the snaphsot is empty
+        print "taking picture !"
+        sleep(0.5)
+        # Killing image capture pipeline
+        self.image_capture.set_state(gst.STATE_NULL)
+        # restoring video pipeline
+        self.video_player.set_state(gst.STATE_PLAYING)
 
 if __name__ == "__main__":
 
+    # Before anything, let's be sure that we have a webcam plugged in
     if not exists('/dev/video0'):
         print "No webcam detected: /dev/video cannot be found.\n The program is now exiting."
         exit()
@@ -135,22 +135,8 @@ if __name__ == "__main__":
     resolution = args['resolution']
     framerate = args['framerate']
     snap_format = args['output_format']
-        
+
+    # Let the show begin 
     Webcam(device, resolution, framerate, snap_format)
     gtk.gdk.threads_init()
     gtk.main()
-
-
-# TODO : 
-#     * Take a picture from the videostream
-#     * Handle arguments passing. Possible arguments
-#         * --device, -d: device (/dev/video0, etc)
-#         * --window-size, -w : window dimensions --> W:H (in px)
-#         * --framerate, -f : numbers of frame per second
-
-# BUGS:
-# BUG#1: ONLY DISPLAYS WEBCAM FEED AFTER I LAUNCHED & CLOSED CHEESE 
-# When it works, I have a message "ON SYNC MESSAGE" --> call on_sync_message(self, bus, message) method
-# see other source code, to see how exiting is handled webcam-wise --> HOW DO WE SHUT DOWN THE WEBCAM ?
-# I tested it in the original (webcam.py.bckp) script : works fine the first time, but doesn't work
-# another time after i hit "Stop"

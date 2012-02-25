2663688517140b04501d4c8ff942848a38bad563
Balthazar Rouberol <rouberol.b@gmail.com>
2012-02-25 14:05:15 +0100

    useless commented __main__ in ssGUI.py
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- ssGUI.py
+++ ssGUI.py
@@ -166,23 +166,3 @@ def snapshot_name():
     mi= str(today.minute)
     s = str(today.second)
     return '%s-%s-%s-%s%s%s' %(y, m, d, h, mi, s)
-    
-# if __name__ == "__main__":
-
-#     # Argument parsing
-#     parser = argparse.ArgumentParser(description='Set video stream window parameters')
-#     parser.add_argument('--device', '-d',type=str, action='store', default='/dev/video0', help="Set the video input device path : /dev/videoX")
-#     parser.add_argument('--resolution', '-r', nargs='+', type=str,  default=['640:480'], 
-#                         choices = ['352:288', '640:480', '800:600', '960:720', '1280:720'],
-#                         help="Set the video stream resolution. Of the form W:H")
-#     parser.add_argument('--output-format', '-o', type=str, action='store', default='jpg', choices=['png','jpeg','jpg'], help="Set the snapshot format")
-
-#     args = vars(parser.parse_args())
-#     device = args['device']
-#     resolution = args['resolution']
-#     snap_format = args['output_format']
-
-#     # Let the show begin 
-#     cam = Webcam(device, resolution, snap_format)
-#     gtk.gdk.threads_init()
-#     gtk.main()
